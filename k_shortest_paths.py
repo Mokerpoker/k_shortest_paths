45f2de8f9b51b49a16ddf618377d1c2b418c50b9
Carlos Hermans <43142718+Mokerpoker@users.noreply.github.com>
2018-11-06 16:08:55 +0100

    Update k_shortest_paths.py
    Working Version

--- k_shortest_paths.py
+++ k_shortest_paths.py
@@ -1,17 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-A NetworkX based implementation of Yen's algorithm for computing K-shortest paths.   
-Yen's algorithm computes single-source K-shortest loopless paths for a 
-graph with non-negative edge cost. For more details, see: 
-http://en.m.wikipedia.org/wiki/Yen%27s_algorithm
-"""
-__author__ = 'Guilherme Maia <guilhermemm@gmail.com>'
-
-__all__ = ['k_shortest_paths']
-
-from heapq import heappush, heappop
-from itertools import count
-
 import networkx as nx
 import copy as cp
 
